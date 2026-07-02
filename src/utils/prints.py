48cc878e085eb6794044acb9c5206d734c52cc9a
ariel-sng <arielsinge@hotmail.com>
2026-07-01 22:53:29 -0300

    Mejora al print_error

--- src/utils/prints.py
+++ src/utils/prints.py
@@ -1,5 +1,7 @@
 from src.models.contract_change_output import ContractChangeOutput
 
+import shutil
+import textwrap
 
 def print_header(title: str) -> None:
     width = 66
@@ -16,13 +18,25 @@ def print_success(message: str = "Proceso finalizado correctamente.") -> None:
     print(f"│ ☑ {message:<{width - 3}}│")
     print(f"└{'─' * width}┘")
 
-
 def print_error(message: str = "Se produjo un error.") -> None:
-    width = 66
+    terminal_width = shutil.get_terminal_size(fallback=(80, 20)).columns
 
-    print(f"┌{'─' * width}┐")
-    print(f"│ ⚠ {message:<{width - 3}}│")
-    print(f"└{'─' * width}┘")
+    prefix = "⚠ "
+    max_content = terminal_width - 4  # │ + espacio + espacio + │
+
+    lines = textwrap.wrap(
+        prefix + message,
+        width=max_content,
+        break_long_words=True
+    )
+
+    width = max(len(line) for line in lines)
+
+    print(f"┌{'─' * (width + 2)}┐")
+    for line in lines:
+        print(f"│ {line:<{width}} │")
+    print(f"└{'─' * (width + 2)}┘")    
+
 
 
 def print_contract_change_output(output: ContractChangeOutput) -> None:
